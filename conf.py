c40072ef3d8b3805d8aec62d21389065799797d0
Shay <37958651+stormycloudy@users.noreply.github.com>
2020-01-06 18:52:21 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
     "enabled": True,
-    "repo": "stormycloudy/stormycloudy.github.io@gh-pages"
+    "repo": "stormycloudy/stormycloudy.github.io@master"
 }
 
 # 站点设置
